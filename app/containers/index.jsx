008166ac56cb3bfd2374f54be21eba98290191c2
run <guangxu2016@foxmail.com>
2018-02-02 18:12:46 +0800

    基本页面形成

--- app/containers/index.jsx
+++ app/containers/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
-import LocalStore from '../util/localStore'
-import { CITYNAME } from '../config/localStoreKey'
-import * as userInfoActionsFromOtherFile from '../actions/userinfo' 
+// import { bindActionCreators } from 'redux'
+// import { connect } from 'react-redux'
+// import LocalStore from '../util/localStore'
+// import { CITYNAME } from '../config/localStoreKey'
+// import * as userInfoActionsFromOtherFile from '../actions/userinfo'
 
 class App extends React.Component {
     constructor(props, context) {
@@ -18,43 +18,14 @@ class App extends React.Component {
         return (
             <div>
                 {
-                    this.state.initDone
-                    ? this.props.children
-                    : <div>正在加载...</div>
+                     this.props.children
+
                 }
             </div>
         )
     }
-    componentDidMount() {
-        // 获取位置信息
-        let cityName = LocalStore.getItem(CITYNAME)
-        if (cityName == null) {
-            cityName = '北京'
-        }
-        this.props.userInfoActions.update({
-            cityName: cityName
-        })
 
-        // 更改状态
-        this.setState({
-            initDone: true
-        })
-    }
+
+
 }
-
-// -------------------redux react 绑定--------------------
-
-function mapStateToProps(state) {
-    return {
-    }
-}
-
-function mapDispatchToProps(dispatch) {
-    return {
-        userInfoActions: bindActionCreators(userInfoActionsFromOtherFile, dispatch),
-    }
-}
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(App)
+export default App
