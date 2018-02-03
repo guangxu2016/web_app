b146aaf52cf4d2a6a657818bae5646fec6a47f63
run <guangxu2016@foxmail.com>
2018-02-03 13:00:16 +0800

    homeheader完成

--- app/components/HomeHeader/index.jsx
+++ app/components/HomeHeader/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 // import PureRenderMixin from 'react-addons-pure-render-mixin'
 // import { Link, hashHistory } from 'react-router'
-//
 // import SearchInput from '../SearchInput'
 
 import './style.css'
@@ -11,16 +10,20 @@ class HomeHeader extends React.Component {
 
     render() {
         return (
-            <div className="clear-fix">
-                <div className="float-left">
-                    北京
+            //home头部
+            <div id="home-header" className="clear-fix">
+                {/*头 左边部分*/}
+                <div className="left top">
+                    <span>北京</span>&nbsp;
                     <i className="icon-angle-down"></i>
                 </div>
-                <div className="headerContext float-left">
+                {/*头 中间部分*/}
+                <div className="context left">
                     <i className="icon-search"></i>
                     <input type="text" placeholder="请输入关键字"/>
                 </div>
-                <div className="float-right ">
+                {/*头 右边部分*/}
+                <div className="right bottom">
                     <i className="icon-user"></i>
                 </div>
             </div>
