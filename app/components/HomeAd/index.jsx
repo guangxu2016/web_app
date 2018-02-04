5521098f658f813e5021a4c5b2d8caab34c4df07
run <guangxu2016@foxmail.com>
2018-02-04 08:38:42 +0800

    特惠完成

--- app/components/HomeAd/index.jsx
+++ app/components/HomeAd/index.jsx
@@ -1,18 +1,28 @@
 import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
 
-import "./homead.css";
+import "./homead.css"
 
 class HomeAd extends React.Component {
     constructor(props, context) {
         super(props, context);
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
     }
-    render() {
-        return (
-            <div id="home-ad">
-                <h2>超值特惠</h2>
 
+    render() {
+        const data = this.props.data
+        return (
+            <div id="home-adv">
+                <h2>超值特惠</h2>
+                <div className="ad-container clear-fix">
+                    {data.map((item, index) => {
+                        return <div key={index} className="ad-item left">
+                            <a href={item.link}>
+                                <img src={item.img} style={{width: "100px"}}/>
+                            </a>
+                        </div>
+                    })}
+                </div>
             </div>
         )
     }
